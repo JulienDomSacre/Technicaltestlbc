a9346eb930a3a8735839dc291f77fdaad3a04759
Goldensnake <juliendom.sacre@gmail.com>
2017-10-14 17:10:45 +0200

    init the toolbar

--- app/src/main/java/com/juliens/technicaltestlbc/listphoto/ListPhotoActivity.java
+++ app/src/main/java/com/juliens/technicaltestlbc/listphoto/ListPhotoActivity.java
@@ -1,7 +1,9 @@
 package com.juliens.technicaltestlbc.listphoto;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.juliens.technicaltestlbc.R;
 import com.juliens.technicaltestlbc.util.ActivityUtils;
@@ -17,9 +19,14 @@ public class ListPhotoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_photo);
 
-        //TODO set the toolbar
+        //set the toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
 
-        //TODO set the fragment
+        //set the fragment
         ListPhotoFragment listPhotoFragment =
                 (ListPhotoFragment) getSupportFragmentManager().findFragmentById(R.id.contentFrame);
         if (listPhotoFragment == null) {
@@ -29,8 +36,7 @@ public class ListPhotoActivity extends AppCompatActivity {
                     getSupportFragmentManager(), listPhotoFragment, R.id.contentFrame);
         }
 
-        //TODO create the presenter
-        // Create the presenter
+        //create the presenter
         mListPhotoPresenter = new ListPhotoPresenter(listPhotoFragment);
     }
 }
