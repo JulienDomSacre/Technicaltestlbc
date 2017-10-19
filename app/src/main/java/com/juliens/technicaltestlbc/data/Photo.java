fc1ca764ff1cd784740636bfd379708e741ca548
Goldensnake <juliendom.sacre@gmail.com>
2017-10-19 19:39:07 +0200

    change to parcelable class Photo

--- app/src/main/java/com/juliens/technicaltestlbc/data/Photo.java
+++ app/src/main/java/com/juliens/technicaltestlbc/data/Photo.java
@@ -1,41 +1,130 @@
 package com.juliens.technicaltestlbc.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by juliens on 12/10/2017.
  */
-
-public class Photo {
-    private int id;
-    private int albumId;
+public class Photo implements Parcelable
+{
+    @SerializedName("albumId")
+    @Expose
+    private Integer albumId;
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("title")
+    @Expose
     private String title;
+    @SerializedName("url")
+    @Expose
     private String url;
+    @SerializedName("thumbnailUrl")
+    @Expose
     private String thumbnailUrl;
+    public final static Parcelable.Creator<Photo> CREATOR = new Creator<Photo>() {
 
-    public Photo(int id, int albumId, String title, String url, String thumbnailUrl) {
-        this.id = id;
+
+        @SuppressWarnings({
+                "unchecked"
+        })
+        public Photo createFromParcel(Parcel in) {
+            return new Photo(in);
+        }
+
+        public Photo[] newArray(int size) {
+            return (new Photo[size]);
+        }
+
+    }
+            ;
+
+    protected Photo(Parcel in) {
+        this.albumId = ((Integer) in.readValue((Integer.class.getClassLoader())));
+        this.id = ((Integer) in.readValue((Integer.class.getClassLoader())));
+        this.title = ((String) in.readValue((String.class.getClassLoader())));
+        this.url = ((String) in.readValue((String.class.getClassLoader())));
+        this.thumbnailUrl = ((String) in.readValue((String.class.getClassLoader())));
+    }
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public Photo() {
+    }
+
+    /**
+     *
+     * @param id
+     * @param title
+     * @param albumId
+     * @param thumbnailUrl
+     * @param url
+     */
+    public Photo(Integer albumId, Integer id, String title, String url, String thumbnailUrl) {
+        super();
         this.albumId = albumId;
+        this.id = id;
         this.title = title;
         this.url = url;
         this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public Integer getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(Integer albumId) {
+        this.albumId = albumId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getUrl() {
         return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 
     public String getThumbnailUrl() {
         return thumbnailUrl;
     }
 
-    public int getId() {
-        return id;
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
     }
 
-    public int getAlbumId() {
-        return albumId;
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(albumId);
+        dest.writeValue(id);
+        dest.writeValue(title);
+        dest.writeValue(url);
+        dest.writeValue(thumbnailUrl);
     }
+
+    public int describeContents() {
+        return 0;
+    }
+
 }
